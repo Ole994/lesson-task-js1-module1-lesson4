96b13875cc505023474c6751cd9749a6fb4b90b9
Ole994 <69842859+Ole994@users.noreply.github.com>
2021-02-05 12:55:53 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,22 @@ const games = [
         released: 2020
     }
 ];
+
+//Q1
+function printSeason (season) {
+    console.log (season);
+}
+printSeason ("summer");
+
+//Q2
+
+function printNumbers (firstNumber, secoundNumber){
+    console.log ("First" + firstNumber, "secound" + secoundNumber)
+}
+printNumbers (10, 14);
+
+//Q3
+function addNumbers (firstNumber, secoundNumber, thirdNumber){
+    const result = firstNumber + secoundNumber + thirdNumber;
+    return result;
+}
