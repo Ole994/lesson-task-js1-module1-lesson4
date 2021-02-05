71dd2eb6304c02096456c28c58f6c401e2e9c540
Ole994 <69842859+Ole994@users.noreply.github.com>
2021-02-05 16:06:49 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,3 +32,58 @@ function addNumbers (firstNumber, secoundNumber, thirdNumber){
     const result = firstNumber + secoundNumber + thirdNumber;
     return result;
 }
+
+const total = addNumbers (5, 10, 15);
+const totalContainer = document.querySelector (".total");
+totalContainer.innerHTML = total;
+
+//Q4
+function createGreeting (name){
+    const greeting = "Hello, my name is" + name +".";
+    return greeting;
+}
+
+const greeting = ("Serral");
+
+const nameContainer = document.querySelector ("#name");
+nameContainer.innerHTML = greeting;
+
+
+//Q5
+function printListItems (list){
+    for (let i=0; i<list.length; i++) {
+        console.log (list[i]);
+    }
+}
+printListItems (sports);
+
+
+
+//Q6
+
+function createGames(gamesList) {
+
+    let html = "";
+
+    for(let i = 0; i < gamesList.length; i++) {
+
+        let releaseYear = "Unknown year";
+
+        if(gamesList[i].released) {
+            releaseYear = gamesList[i].released;
+        }
+
+        html += `<div class="game">
+                    <h5>${gamesList[i].name}</h5>
+                    <p>Released: ${releaseYear}</p>
+                </div>`;
+    }
+
+    return html;
+}
+
+const newHTML = createGames(games);
+
+const gameContainer = document.querySelector(".game-container");
+gameContainer.innerHTML = newHTML;
+
